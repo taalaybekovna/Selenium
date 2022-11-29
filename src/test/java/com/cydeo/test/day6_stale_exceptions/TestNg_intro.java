89430e805f312aca2c0354f62aa56e4494bdd2c9
Eliza Turgunbaeva <elizaturgunbaeva@gmail.com>
2022-11-29 10:54:38 -0500

    TestNg intro

---
+++ src/test/java/com/cydeo/test/day6_stale_exceptions/TestNg_intro.java
@@ -0,0 +1,15 @@
+package com.cydeo.test.day6_stale_exceptions;
+
+import org.testng.annotations.Test;
+
+public class TestNg_intro {
+
+    @Test
+    public void test(){
+        System.out.println("Testing is starting");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Testing is going on");
+    }
+}
